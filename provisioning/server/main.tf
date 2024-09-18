ba3d8dd54a36a5aa39355460cb31790ca41568f7
david-slatinek <david.slatinek5@gmail.com>
2024-09-18 17:11:35 +0200

    update: set db to local

--- provisioning/server/main.tf
+++ provisioning/server/main.tf
@@ -76,6 +76,20 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   }
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_postgres" {
+  security_group_id = aws_security_group.security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 5432
+  ip_protocol       = "tcp"
+  to_port           = 5432
+
+  tags = {
+    Name        = "${local.name}-postgres"
+    Protocol    = "postgres"
+    Environment = var.env
+  }
+}
+
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.security_group.id
   cidr_ipv4         = "0.0.0.0/0"
@@ -90,7 +104,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
 
 resource "aws_instance" "api" {
   ami                         = data.aws_ami.debian.id
-  instance_type               = "t2.medium"
+  instance_type               = "t3.medium"
   associate_public_ip_address = true
   key_name                    = aws_key_pair.key_pair.key_name
 
